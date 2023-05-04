06d2700a96a9c11c163f7e5224ed5eeed23d7be3
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-04 18:17:45 +0300

    1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (64 < character && character < 91 || character > 96 && character < 123) {
+            if (character == 97 || character == 101 || character == 105 || character == 111
+                    || character == 117 || character == 121 || character == 65 || character == 69 || character == 73
+                    || character == 79 || character == 85 || character == 89) {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
